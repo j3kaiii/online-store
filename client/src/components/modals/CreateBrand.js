9f483c1a6785a01c88deb661281d78d8bcce6321
j3kaiii <j3kaiii@gmail.com>
2021-05-04 18:45:49 +0300

    creating types, brands and devices with info

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Form, Modal} from "react-bootstrap";
+import {createBrand, createType} from "../../http/deviceAPI";
 
 const CreateBrand = ({show, onHide}) => {
+    const [brandValue, setBrandValue] = useState('')
+    const addBrand = () => {
+        createBrand({name: brandValue}).then(data => setBrandValue(''))
+        onHide()
+    }
+
     return (
         <Modal
             show = {show}
@@ -17,13 +24,15 @@ const CreateBrand = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Form.Control
+                        value={brandValue}
+                        onChange={e => setBrandValue(e.target.value)}
                         placeholder={'Введите название бренда'}
                     />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
-                <Button variant='outline-success' onClick={onHide}>Добавить</Button>
+                <Button variant='outline-success' onClick={addBrand}>Добавить</Button>
             </Modal.Footer>
         </Modal>
     );
--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Form, Modal} from "react-bootstrap";
+import {createType} from "../../http/deviceAPI";
 
 const CreateType = ({show, onHide}) => {
+    const [typeValue, setTypeValue] = useState('')
+    const addType = () => {
+        createType({name: typeValue}).then(data => setTypeValue(''))
+        onHide()
+    }
+
     return (
         <Modal
             show = {show}
@@ -17,13 +24,15 @@ const CreateType = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Form.Control
+                        value = {typeValue}
+                        onChange={e => setTypeValue(e.target.value)}
                         placeholder={'Введите название типа'}
                     />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
-                <Button variant='outline-success' onClick={onHide}>Добавить</Button>
+                <Button variant='outline-success' onClick={addType}>Добавить</Button>
             </Modal.Footer>
         </Modal>
     );
